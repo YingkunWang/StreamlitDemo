ba2878289dbaf12c876e6ab41d99acbe7831409a
YingkunWang <49181761+YingkunWang@users.noreply.github.com>
2021-05-21 13:44:41 -0400

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,8 +1,13 @@
 mkdir -p ~/.streamlit/
+
+echo "\
+[general]\n\
+email = \"wangkk0707@domain.com\"\n\
+" > ~/.streamlit/credentials.toml
+
 echo "\
 [server]\n\
 headless = true\n\
+enableCORS=false\n\
 port = $PORT\n\
-enableCORS = false\n\
-\n\
 " > ~/.streamlit/config.toml
